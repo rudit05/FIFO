2f0047888c1de9c501a3bcad8c7ca975de5ced85
rudit05 <166696260+rudit05@users.noreply.github.com>
2026-07-19 02:34:03 +0530

    Update mod_b.v

--- mod_b.v
+++ mod_b.v
@@ -1,5 +1,5 @@
 module mod_b (
-    input clk, rst,
+    input clk, rst, empty,
     input [7:0] data_in,
     output reg [7:0] data_out,
     output reg rd_en
@@ -7,27 +7,30 @@ module mod_b (
     parameter idle = 2'b00, s1 = 2'b01, data_state = 2'b10;
     reg [1:0] ps, ns;
 
-
     always @(posedge clk) begin
         if (rst) ps <= idle;
-        else ps <= ns;
+        else if (!empty) ps <= ns;
     end
-    
+
     always @(*) begin
-        case (ps)
-            idle: begin
-                    ns = s1; 
-                    rd_en = 0;
-                  end
-            s1:  begin 
-                    ns = data_state;  
-                  end
-            data_state: begin 
-                    ns = idle; 
-                    rd_en = 1; 
-                    data_out = data_in;
-                  end 
-        endcase
+        if (!empty) begin
+         case (ps)
+                idle: begin
+                        ns = s1; 
+                        rd_en = 0;
+                     end
+               s1:  begin 
+                        ns = data_state;  
+                      end
+              data_state: begin 
+                        ns = idle; 
+                        rd_en = 1; 
+                       data_out = data_in;
+                     end 
+              default : ns = idle;
+            endcase
+        end
+        
     end
 
 endmodule
