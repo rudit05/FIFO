0e565979dbeb78f7ed6ab990f05e9372bb6eab58
rudit05 <166696260+rudit05@users.noreply.github.com>
2026-07-19 02:34:48 +0530

    Update fifo_8_8.v

--- fifo_8_8.v
+++ fifo_8_8.v
@@ -4,9 +4,11 @@ module fifo_8_8(
     output full, empty
     );
     
-    reg [2:0] wr_ptr=0, rd_ptr=0; 
+    reg [2:0] wr_ptr=0, rd_ptr=0;
+    reg [3:0] count=0; 
     
-    reg [7:0] mem [0:7]; 
+    reg [7:0] mem [0:7];
+    wire [1:0] fifo_case;
     
     integer i;
     
@@ -16,19 +18,34 @@ module fifo_8_8(
                 begin  
                     for(i=0; i<=7; i=i+1)
                         mem[i] <= 0;
+                    wr_ptr <= 0;
+                    rd_ptr <= 0;
+                    count <= 0;
+                    data_out <= 0;
                 end
-            if (wr_en && (full==0)) 
-                begin
-                    mem[wr_ptr] <= data_in;
-                    wr_ptr <= wr_ptr + 1'b1;
+            case ({wr_en && !full, rd_en && !empty})
+                2'b10: begin
+                     mem[wr_ptr] <= data_in;
+                     wr_ptr <= wr_ptr + 1;
+                     count <= count + 1;
                 end
-            if(rd_en && (empty == 0)) 
-                begin
+                2'b01: begin
                     data_out <= mem[rd_ptr];
-                    rd_ptr <= rd_ptr + 1'b1;
+                    rd_ptr <= rd_ptr + 1;
+                    count <= count - 1;
                 end
+                2'b11: begin
+                    mem[wr_ptr] <= data_in;
+                    data_out <= mem[rd_ptr];
+                    wr_ptr <= wr_ptr + 1;
+                    rd_ptr <= rd_ptr + 1;
+                end
+                2'b00: begin
+                end
+            endcase
         end
-        
-        assign full =  ((wr_ptr + 1'b1) == rd_ptr) ? 1'b1 : 1'b0; 
-        assign empty = (wr_ptr == rd_ptr);                
+
+        assign full =  (count == 4'd8); 
+        assign empty = (count == 0);   
+        assign fifo_case = {wr_en && !full, rd_en && !empty};        
 endmodule
